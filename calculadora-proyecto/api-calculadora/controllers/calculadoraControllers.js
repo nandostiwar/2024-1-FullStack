887c95da8d0afb3384fcc3f6e57192d249df79e2
DavidMartinez0 <110999808+DavidMartinez0@users.noreply.github.com>
2024-02-21 23:50:43 -0500

    ACTUALIZACION OPERACIONES

--- calculadora-proyecto/api-calculadora/controllers/calculadoraControllers.js
+++ calculadora-proyecto/api-calculadora/controllers/calculadoraControllers.js
@@ -1,27 +1,45 @@
 const {add, subtract, multiply} = require('../operaciones/operaciones.js');
 
 function sumar(req, res){
-    const {body} = req;
+    console.log(req)
+    const body = req;
     const {number1, number2} = body;
     const result = add(number1, number2);
-    res.json({
-        resultado: result
-    });
+    return result;
 }
 
 function restar(req, res){
-    const {body} = req;
+    const body = req;
     const {number1, number2} = body;
     const result = subtract(number1, number2);
-    res.json({
-        resultado: result
-    })
+    return result;
 }
 
 function multiplicar(req, res){
-    const {body} = req;
+    const body = req;
     const {number1, number2} = body;
     const result = multiply(number1, number2);
+    return result;
+}
+
+function operaciones(req, res){
+    const {body} = req;
+    const {operacion, number1, number2} = body;
+    console.log(body);
+    let result;
+
+    switch (operacion) {
+        case 'sumar':
+            result = sumar({number1, number2})
+            break;
+        case 'restar':
+            result = restar({number1, number2});
+            break;
+        case 'multiplicar':
+            result = multiplicar({number1, number2});
+            break; 
+    }
+
     res.json({
         resultado: result
     })
@@ -30,5 +48,6 @@ function multiplicar(req, res){
 module.exports = {
     sumar,
     restar,
-    multiplicar
+    multiplicar,
+    operaciones
 }
