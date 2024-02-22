08b2ff4214a3a1136e5b80c4b8b6c1159d5fb01c
Juan David <juandavidquijano70@gmail.com>
2024-02-22 00:43:21 -0500

    si cambio de nombre

--- calculadora-proyecto/api-calculadora/controllers/calculadoraControllers.js
+++ calculadora-proyecto/api-calculadora/controllers/calculadoraControllers.js
@@ -1,34 +1,79 @@
-const {add, subtract, multiply} = require('../operaciones/operaciones.js');
+const {
+    add,
+    subtract,
+    multiply
+} = require('../operaciones/operaciones.js');
 
-function sumar(req, res){
-    const {body} = req;
-    const {number1, number2} = body;
+function sumar(req, res) {
+    
+    const {
+        number1,
+        number2
+    } = req;
     const result = add(number1, number2);
-    res.json({
-        resultado: result
-    });
+    return result;
 }
 
-function restar(req, res){
-    const {body} = req;
-    const {number1, number2} = body;
+function restar(req, res) {
+    
+    const {
+        number1,
+        number2
+    } = req;
     const result = subtract(number1, number2);
-    res.json({
-        resultado: result
-    })
+    return result;
 }
 
-function multiplicar(req, res){
-    const {body} = req;
-    const {number1, number2} = body;
+function multiplicar(req, res) {
+    console.log(req);
+    
+    const {
+        number1,
+        number2
+    } = req;
     const result = multiply(number1, number2);
-    res.json({
-        resultado: result
-    })
+    return result;
+}
+
+function operaciones(req, res) {
+    const {
+        body
+    } = req;
+    const{number1, number2, operacion} = body;
+    console.log(operacion);
+    var resultado;
+    switch (operacion) {
+        case 'sumar':
+            resultado = sumar({
+                number1,
+                number2
+            });
+
+            break;
+
+        case 'restar':
+            resultado = restar({
+                number1,
+                number2
+            });
+
+            break;
+        case 'multiplicar':
+            resultado = multiplicar({
+                number1,
+                number2
+            });
+
+            break;
+
+    }
+    res.json({resultado});
+
 }
 
 module.exports = {
     sumar,
     restar,
-    multiplicar
+    multiplicar,
+    operaciones
 }
