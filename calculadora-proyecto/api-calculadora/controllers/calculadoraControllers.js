4c144fb5f804aa4eec74c7b65f60af1aac842689
jhonatan.munoz <jhonatan.munoz02@unicatolica.edu.co>
2024-02-21 20:48:56 -0500

    creo endpoint operar

--- calculadora-proyecto/api-calculadora/controllers/calculadoraControllers.js
+++ calculadora-proyecto/api-calculadora/controllers/calculadoraControllers.js
@@ -1,34 +1,58 @@
 const {add, subtract, multiply} = require('../operaciones/operaciones.js');
 
-function sumar(req, res){
+function operacion(req, res){
     const {body} = req;
-    const {number1, number2} = body;
-    const result = add(number1, number2);
+    const {operacion, number1, number2} = body;
+    let result;
+
+    switch (operacion) {
+        case "sumar":
+            result = sumar({number1, number2});
+            break;
+        case "restar":
+            result = restar({number1, number2});
+            break;
+        case "multiplicar":
+            result = multiplicar({number1, number2});
+            break;
+        default:
+            break;
+    }
+
     res.json({
         resultado: result
     });
+
+
 }
 
-function restar(req, res){
-    const {body} = req;
+function sumar(req){
+    const body = req;
+    const {number1, number2} = body;
+    const result = add(number1, number2);
+
+    return result;
+}
+
+function restar(req){
+    const body = req;
     const {number1, number2} = body;
     const result = subtract(number1, number2);
-    res.json({
-        resultado: result
-    })
+
+    return result;
 }
 
-function multiplicar(req, res){
-    const {body} = req;
+function multiplicar(req){
+    const body = req;
     const {number1, number2} = body;
     const result = multiply(number1, number2);
-    res.json({
-        resultado: result
-    })
+
+    return result;
 }
 
 module.exports = {
     sumar,
     restar,
-    multiplicar
+    multiplicar,
+    operacion
 }
