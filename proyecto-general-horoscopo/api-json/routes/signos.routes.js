a2e15de926eeffe8b7a8d0ce6febaadb221a026e
DualiDaD69 <miguel.duque01@unicatolica.edu.co>
2024-03-13 01:23:03 -0500

    Horoscopo fullstack

--- proyecto-general-horoscopo/api-json/routes/signos.routes.js
+++ proyecto-general-horoscopo/api-json/routes/signos.routes.js
@@ -4,6 +4,6 @@ const signoController = require('./controllers/signoController.js');
 router
     .get('/', signoController.getAllSignos)
     .get('/:signo', signoController.getOneSigno)
-    .patch('/:signoEditar', signoController.updateSigno)
+    .post('/signoEditar', signoController.updateSigno)
 
 module.exports = router;
