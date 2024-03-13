999a6bd5052f7427d69982f6e7999f1397a37ce5
Joan Umaña <160068768+JoanUmana1209@users.noreply.github.com>
2024-03-12 23:59:10 -0500

    Calculadora Cambio API

--- calculadora-proyecto/api-calculadora/controllers/calculadoraControllers.js
+++ calculadora-proyecto/api-calculadora/controllers/calculadoraControllers.js
@@ -1,27 +1,48 @@
 const {add, subtract, multiply} = require('../operaciones/operaciones.js');
 
 function sumar(req, res){
-    const {body} = req;
+    console.log(req);
+    const body = req;
     const {number1, number2} = body;
     const result = add(number1, number2);
-    res.json({
-        resultado: result
-    });
+    return result;
 }
 
 function restar(req, res){
-    const {body} = req;
+    const body = req;
     const {number1, number2} = body;
     const result = subtract(number1, number2);
-    res.json({
-        resultado: result
-    })
+    return result;
 }
 
 function multiplicar(req, res){
-    const {body} = req;
+    const body = req;
     const {number1, number2} = body;
     const result = multiply(number1, number2);
+    return result;
+}
+
+function operaciones(req, res){
+    const {body} = req;
+    const {operacion, number1, number2} = body;
+    console.log(body);
+    let result;
+
+    switch (operacion) {
+        case 'sumar':
+            result = sumar({number1, number2})
+            break;
+        case 'restar':
+            result = restar({number1, number2})
+            break;
+        case 'multiplicar':
+            result = multiplicar({number1, number2})
+            break;
+        default:
+            res.status(400).json({ error: 'Operación no válida' });
+            return;
+    }
+
     res.json({
         resultado: result
     })
@@ -30,5 +51,6 @@ function multiplicar(req, res){
 module.exports = {
     sumar,
     restar,
-    multiplicar
+    multiplicar,
+    operaciones
 }
